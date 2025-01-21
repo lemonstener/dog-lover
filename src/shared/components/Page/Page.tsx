7699a2c68c2fb4d16ebfa3e5fb902f579eabb0c5
lemonstener <deyanvasilev7@gmail.com>
2025-01-20 21:25:19 -0600

    more styling

--- src/shared/components/Page/Page.tsx
+++ src/shared/components/Page/Page.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from "react"
 import { NavBar } from "./NavBar";
+import { Box } from "@mui/material";
 
 export const Page = ({ title, children }: { title: string, children: ReactNode }) => {
     document.title = `Dog Lover - ${title}`;
 
     return (
-        <>
+        <Box sx={{ height: '100vh', position: 'relative' }}>
             <NavBar />
             {children}
-        </>
+        </Box>
     )
 }
