b55561596c6faad51e0e0e25e47d8b8ce70652b3
lemonstener <deyanvasilev7@gmail.com>
2025-01-22 14:05:12 -0600

    add a "Reset all" button for filters

--- src/features/search/components/SearchEngine.tsx
+++ src/features/search/components/SearchEngine.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent } from "@mui/material";
+import { Box, Button, Card, CardContent, useMediaQuery, useTheme } from "@mui/material";
 import { BreedFilter } from "./BreedFilter";
 import { ZipCodeFilter } from "./ZipCodeFilter";
 import { AgeMinFilter } from "./AgeMinFilter";
@@ -9,10 +9,20 @@ import { SortButton } from "./SortButton";
 import { SizeButton } from "./SizeButton";
 import { useSearchParams } from "react-router";
 import { searchDefaultValues } from "../utils/searchDefaultValues";
+import { createParams } from "../utils/createParams";
+import { AllFilters } from "../types/AllFilters";
 
 export const SearchEngine = () => {
     const [searchParams,] = useSearchParams();
     const { breeds, zipCodes, ageMin, ageMax } = searchDefaultValues(searchParams)
+    const { breakpoints } = useTheme();
+    const isLargeScreen = useMediaQuery(breakpoints.up('lg'))
+    const [, setSearchParams] = useSearchParams();
+
+    const resetFilters = () => {
+        const newParams = createParams({} as AllFilters);
+        setSearchParams(newParams);
+    };
 
     return (
         <Card sx={{ border: '.2px solid gray', mt: 0 }}>
@@ -43,6 +53,13 @@ export const SearchEngine = () => {
                     {ageMax && <FilterChip label={`Max ${ageMax} years old`} type={Filter.AgeMax} />}
                     {ageMin && <FilterChip label={`Min ${ageMin} years old`} type={Filter.AgeMin} />}
                 </Box>
+                <Button
+                    variant={isLargeScreen ? 'outlined' : 'contained'}
+                    sx={{ alignSelf: 'center' }}
+                    onClick={resetFilters}
+                >
+                    Reset all
+                </Button>
             </CardContent>
         </Card>
     )
