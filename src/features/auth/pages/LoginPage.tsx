ccd378639a08e57aa6ced450b8fa79d80476c0de
lemonstener <deyanvasilev7@gmail.com>
2025-01-23 09:23:04 -0600

    remove yup and resolvers, use native HTML validation

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -3,8 +3,6 @@ import { Controller, useForm } from "react-hook-form";
 import { useLogin } from "../hooks/useLogin";
 import { useNavigate } from "react-router";
 import toast from "react-hot-toast";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { loginFormSchema } from "../schemas/loginFormSchema";
 import { useEffect } from "react";
 
 const LoginPage = () => {
@@ -12,9 +10,12 @@ const LoginPage = () => {
     const navigate = useNavigate();
     const { mutate, isLoading, isSuccess } = useLogin();
 
+    // No need for using a schema package like YUP, because:
+    // 1. We only have one form that needs validation in the entire app
+    // 2. Yup's built-in email validator does not match the server's email pattern, so we'd need to make our own anyway
+
     const { control, handleSubmit, formState: { errors } } = useForm({
-        defaultValues: { name: '', email: '' },
-        resolver: yupResolver(loginFormSchema)
+        defaultValues: { name: '', email: '' }
     });
 
     useEffect(() => {
@@ -39,6 +40,9 @@ const LoginPage = () => {
                 <Controller
                     control={control}
                     name={'name'}
+                    rules={{
+                        required: { value: true, message: 'Name is required' }
+                    }}
                     render={({ field }) => {
                         return (
                             <TextField error={!!errors.name} helperText={errors.name?.message} label={'Name'} {...field} />
@@ -47,6 +51,13 @@ const LoginPage = () => {
                 <Controller
                     control={control}
                     name={'email'}
+                    rules={{
+                        required: { value: true, message: 'Email is required' },
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: 'Invalid email'
+                        }
+                    }}
                     render={({ field }) => {
                         return (
                             <TextField error={!!errors.email} helperText={errors.email?.message} label={'Email'} {...field} />
--- src/features/auth/schemas/loginFormSchema.ts
+++
@@ -1,6 +0,0 @@
-import { object, string } from "yup";
-
-export const loginFormSchema = object().shape({
-    name: string().required('Name is required'),
-    email: string().email('Invalid email pattern').required('Email is required')
-})
