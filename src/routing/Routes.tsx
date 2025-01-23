110ea9cee82c5cf97ec6cc5c090e927396822277
lemonstener <deyanvasilev7@gmail.com>
2025-01-22 21:52:07 -0600

    restore hashRouter and protected routing

--- src/routing/Routes.tsx
+++ src/routing/Routes.tsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
+import { HashRouter, Navigate, Route, Routes } from 'react-router';
+import { ProtectedRoutes } from './ProtectedRoutes';
 
 export const AppRoutes = () => {
     const Login = lazy(() => import('../features/auth/pages/LoginPage'));
@@ -9,18 +10,19 @@ export const AppRoutes = () => {
     const Favorites = lazy(() => import('../features/search/pages/FavoritesPage'));
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/logout" element={<Logout />} />
 
-                    <Route path='/' element={<Search />} />
-                    <Route path="/favorites" element={<Favorites />} />
-                    <Route path="*" element={<Navigate to="/" replace />} />
-
+                    <Route element={<ProtectedRoutes />}>
+                        <Route path='/' element={<Search />} />
+                        <Route path="/favorites" element={<Favorites />} />
+                        <Route path="*" element={<Navigate to="/" replace />} />
+                    </Route>
                 </Routes>
             </Suspense>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
